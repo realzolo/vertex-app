70342765898c574dbc35994b1abc70da1fbfd6aa
lixm <zolo@onezol.com>
2024-10-30 22:57:48 +0800

    使用ContiNew Admin UI

---
+++ app-web/src/components/GiForm/src/GiForm.vue
@@ -0,0 +1,225 @@
+<template>
+  <a-form ref="formRef" :auto-label-width="true" v-bind="options.form" :model="modelValue">
+    <a-grid class="w-full" :col-gap="8" v-bind="options.grid" :collapsed="collapsed">
+      <template v-for="(item, index) in columns" :key="item.field">
+        <a-grid-item
+          v-if="!isHide(item.hide)" v-show="colVShow(index)" v-bind="item.gridItemProps || props.options.gridItem"
+          :span="item.span || options.gridItem?.span"
+        >
+          <a-form-item
+            v-bind="item.formItemProps" :label="item.label" :field="item.field" :rules="item.rules"
+            :disabled="isDisabled(item.disabled)"
+          >
+            <slot
+              v-if="!['group-title'].includes(item.type || '')" :name="item.field"
+              v-bind="{ disabled: isDisabled(item.disabled) }"
+            >
+              <template v-if="item.type === 'range-picker'">
+                <DateRangePicker
+                  v-bind="(item.props as A.RangePickerInstance['$props'])"
+                  :model-value="modelValue[item.field as keyof typeof modelValue]"
+                  @update:model-value="valueChange($event, item.field)"
+                />
+              </template>
+              <component
+                :is="`a-${item.type}`" v-else v-bind="getComponentBindProps(item)"
+                :model-value="modelValue[item.field as keyof typeof modelValue]"
+                @update:model-value="valueChange($event, item.field)"
+              ></component>
+            </slot>
+            <slot v-else name="group-title">
+              <a-alert v-bind="item.props">{{ item.label }}</a-alert>
+            </slot>
+          </a-form-item>
+        </a-grid-item>
+      </template>
+      <a-grid-item v-if="!options.btns?.hide" :suffix="options.fold?.enable">
+        <a-space wrap :size="[8, 16]" style="flex-wrap: nowrap">
+          <slot name="suffix">
+            <a-button type="primary" @click="emit('search')">
+              <template #icon><icon-search /></template>
+              <template #default>{{ options.btns?.searchBtnText || '查询' }}</template>
+            </a-button>
+            <a-button @click="emit('reset')">
+              <template #icon><icon-refresh /></template>
+              <template #default>重置</template>
+            </a-button>
+            <a-button
+              v-if="options.fold?.enable" class="gi-form__fold-btn" type="text" size="mini"
+              @click="collapsed = !collapsed"
+            >
+              <template #icon>
+                <icon-up v-if="!collapsed" />
+                <icon-down v-else />
+              </template>
+              <template #default>{{ collapsed ? '展开' : '收起' }}</template>
+            </a-button>
+          </slot>
+        </a-space>
+      </a-grid-item>
+    </a-grid>
+  </a-form>
+</template>
+
+<script setup lang="ts">
+import { cloneDeep } from 'lodash-es'
+import type { ColumnsItem, ColumnsItemDisabled, ColumnsItemHide, Options } from './type'
+
+interface Props {
+  modelValue: any
+  options?: Options
+  columns: ColumnsItem[]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  options: () => ({}),
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: any): void
+  (e: 'search'): void
+  (e: 'reset'): void
+}>()
+
+const options = computed(() => ({
+  grid: { cols: 1 },
+  gridItem: { span: { xs: 2, sm: 1 } },
+  ...props.options,
+}
+))
+
+const formRef = useTemplateRef('formRef')
+const collapsed = ref(props.options.fold?.defaultCollapsed ?? false)
+const dicData: Record<string, any> = reactive({})
+
+// col组件的显示隐藏
+const colVShow = (index: number) => {
+  return index <= (props.options.fold?.index || 0) || (index >= (props.options.fold?.index || 0) && !collapsed.value)
+}
+
+// 组件的默认props配置
+const getComponentBindProps = (item: ColumnsItem) => {
+  const obj: Partial<ColumnsItem['props'] & { placeholder: string }> = {}
+  if (item.type === 'input') {
+    obj.allowClear = true
+    obj.placeholder = `请输入${item.label}`
+  }
+  if (item.type === 'input-password') {
+    obj.allowClear = true
+    obj.placeholder = `请输入${item.label}`
+  }
+  if (item.type === 'input-number') {
+    obj.allowClear = true
+    obj.placeholder = `请输入${item.label}`
+  }
+  if (item.type === 'textarea') {
+    obj.placeholder = `请输入${item.label}`
+    obj.maxLength = 200
+  }
+  if (item.type === 'select') {
+    obj.allowClear = true
+    obj.placeholder = `请选择${item.label}`
+    obj.options = dicData[item.field] || item.options
+  }
+  if (item.type === 'cascader') {
+    obj.allowClear = true
+    obj.placeholder = `请选择${item.label}`
+    obj.options = dicData[item.field] || item.options
+  }
+  if (item.type === 'tree-select') {
+    obj.allowClear = true
+    obj.placeholder = `请选择${item.label}`
+    obj.data = dicData[item.field] || item.data
+  }
+  if (item.type === 'radio-group') {
+    obj.options = dicData[item.field] || item.options
+  }
+  if (item.type === 'checkbox-group') {
+    obj.options = dicData[item.field] || item.options
+  }
+  if (item.type === 'date-picker') {
+    obj.placeholder = '请选择日期'
+  }
+  if (item.type === 'time-picker') {
+    obj.allowClear = true
+    obj.placeholder = `请选择时间`
+  }
+  return { ...obj, ...item.props }
+}
+
+const valueChange = (value: any, field: string) => {
+  emit('update:modelValue', Object.assign(props.modelValue, { [field]: value }))
+}
+
+const isHide = (hide?: ColumnsItemHide<boolean | object>) => {
+  if (hide === undefined) return false
+  if (typeof hide === 'boolean') return hide
+  if (typeof hide === 'function') {
+    return hide(props.modelValue)
+  }
+}
+
+const isDisabled = (disabled?: ColumnsItemDisabled<boolean | object>) => {
+  if (disabled === undefined) return false
+  if (typeof disabled === 'boolean') return disabled
+  if (typeof disabled === 'function') {
+    return disabled(props.modelValue)
+  }
+}
+
+defineExpose({ formRef })
+
+props.columns.forEach((item) => {
+  if (item.request && typeof item.request === 'function' && item?.init) {
+    item.request(props.modelValue).then((res) => {
+      dicData[item.field] = item.resultFormat ? item.resultFormat(res) : res.data
+      // console.log('dicData', dicData)
+    })
+  }
+})
+
+// 先找出有级联的项
+// 如果这个字段改变了值，那么就找出它的cascader属性对应的字段项，去请求里面的request
+const hasCascaderColumns: ColumnsItem[] = []
+props.columns.forEach((item) => {
+  const arr = hasCascaderColumns.map((i) => i.field)
+  if (item.cascader?.length && !arr.includes(item.field)) {
+    hasCascaderColumns.push(item)
+  }
+})
+
+// 要深克隆，否则无法监听新旧值变化
+const cloneForm = computed(() => cloneDeep(props.modelValue))
+
+watch(cloneForm as any, (newVal, oldVal) => {
+  hasCascaderColumns.forEach((item) => {
+    if (newVal[item.field] !== oldVal[item.field]) {
+      const arr = props.columns.filter((a) => {
+        return item?.cascader?.includes(a.field)
+      })
+      arr.forEach((i) => {
+        if (i.request && Boolean(newVal[item.field])) {
+          i.request(props.modelValue).then((res) => {
+            dicData[i.field] = i.resultFormat ? i.resultFormat(res) : res.data
+            if (!dicData[i.field].map((i: any) => i.value).includes(props.modelValue[i.field])) {
+              emit('update:modelValue', Object.assign(props.modelValue, { [i.field]: '' }))
+            }
+          })
+        } else if (i.request && !newVal[item.field]) {
+          dicData[i.field] = []
+          emit('update:modelValue', Object.assign(props.modelValue, { [i.field]: '' }))
+        }
+      })
+    }
+  })
+})
+</script>
+
+<style lang="scss" scoped>
+:deep(.arco-form-item-layout-inline) {
+  margin-right: 0;
+}
+.gi-form__fold-btn {
+  padding: 0 5px;
+}
+</style>
---
+++ app-web/src/views/system/user/index.vue
@@ -0,0 +1,266 @@
+<template>
+  <div class="table-page">
+    <a-row justify="space-between" align="center" class="header page_header">
+      <a-space wrap>
+        <div class="title">用户管理</div>
+      </a-space>
+    </a-row>
+    <a-row align="stretch" :gutter="14" class="h-full page_content">
+      <a-col :xs="0" :sm="0" :md="6" :lg="5" :xl="5" :xxl="4" class="h-full ov-hidden">
+        <DeptTree placeholder="请输入名称" @node-click="handleSelectDept" />
+      </a-col>
+      <a-col :xs="24" :sm="24" :md="18" :lg="19" :xl="19" :xxl="20" class="h-full ov-hidden">
+        <GiTable
+          row-key="id"
+          :data="dataList"
+          :columns="columns"
+          :loading="loading"
+          :scroll="{ x: '100%', y: '100%', minWidth: 1500 }"
+          :pagination="pagination"
+          :disabled-tools="['size']"
+          :disabled-column-keys="['nickname']"
+          @refresh="search"
+        >
+          <template #top>
+            <GiForm v-model="queryForm" :options="options" :columns="queryFormColumns" @search="search" @reset="reset"></GiForm>
+          </template>
+          <template #toolbar-left>
+            <a-button v-permission="['system:user:add']" type="primary" @click="onAdd">
+              <template #icon><icon-plus /></template>
+              <template #default>新增</template>
+            </a-button>
+            <a-button v-permission="['system:user:import']" @click="onImport">
+              <template #icon><icon-upload /></template>
+              <template #default>导入</template>
+            </a-button>
+          </template>
+          <template #toolbar-right>
+            <a-button v-permission="['system:user:export']" @click="onExport">
+              <template #icon><icon-download /></template>
+              <template #default>导出</template>
+            </a-button>
+          </template>
+          <template #nickname="{ record }">
+            <GiCellAvatar :avatar="record.avatar" :name="record.nickname" />
+          </template>
+          <template #gender="{ record }">
+            <GiCellGender :gender="record.gender" />
+          </template>
+          <template #roleNames="{ record }">
+            <GiCellTags :data="record.roleNames" />
+          </template>
+          <template #status="{ record }">
+            <GiCellStatus :status="record.status" />
+          </template>
+          <template #isSystem="{ record }">
+            <a-tag v-if="record.isSystem" color="red" size="small">是</a-tag>
+            <a-tag v-else color="arcoblue" size="small">否</a-tag>
+          </template>
+          <template #action="{ record }">
+            <a-space>
+              <a-link v-permission="['system:user:list']" @click="onDetail(record)">详情</a-link>
+              <a-link v-permission="['system:user:update']" @click="onUpdate(record)">修改</a-link>
+              <a-link
+                v-permission="['system:user:delete']"
+                status="danger"
+                :title="record.isSystem ? '系统内置数据不能删除' : '删除'"
+                :disabled="record.disabled"
+                @click="onDelete(record)"
+              >
+                删除
+              </a-link>
+              <a-dropdown>
+                <a-button v-if="has.hasPermOr(['system:user:resetPwd'])" type="text" size="mini">
+                  <template #icon>
+                    <icon-more :size="16" />
+                  </template>
+                </a-button>
+                <template #content>
+                  <a-doption v-permission="['system:user:resetPwd']" @click="onResetPwd(record)">重置密码</a-doption>
+                </template>
+              </a-dropdown>
+            </a-space>
+          </template>
+        </GiTable>
+      </a-col>
+    </a-row>
+
+    <UserAddDrawer ref="UserAddDrawerRef" @save-success="search" />
+    <UserImportDrawer ref="UserImportDrawerRef" @save-success="search" />
+    <UserDetailDrawer ref="UserDetailDrawerRef" />
+    <UserResetPwdModal ref="UserResetPwdModalRef" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DeptTree from './dept/index.vue'
+import UserAddDrawer from './UserAddDrawer.vue'
+import UserImportDrawer from './UserImportDrawer.vue'
+import UserDetailDrawer from './UserDetailDrawer.vue'
+import UserResetPwdModal from './UserResetPwdModal.vue'
+import { type UserQuery, type UserResp, deleteUser, exportUser, listUser } from '@/apis/system'
+import type { Columns, Options } from '@/components/GiForm'
+import type { TableInstanceColumns } from '@/components/GiTable/type'
+import { useDownload, useTable } from '@/hooks'
+import { isMobile } from '@/utils'
+import has from '@/utils/has'
+import { DisEnableStatusList } from '@/constant/common'
+
+defineOptions({ name: 'SystemUser' })
+
+const queryForm = reactive<UserQuery>({
+  sort: ['t1.createTime,desc'],
+})
+const {
+  tableData: dataList,
+  loading,
+  pagination,
+  search,
+  handleDelete,
+} = useTable((page) => listUser({ ...queryForm, ...page }), { immediate: false })
+
+const options: Options = reactive({
+  form: { layout: 'inline' },
+  grid: { cols: { xs: 1, sm: 1, md: 2, lg: 3, xl: 3, xxl: 3 } },
+  fold: { enable: true, index: 1, defaultCollapsed: true },
+})
+
+const queryFormColumns: Columns = reactive([
+  {
+    type: 'input',
+    field: 'description',
+    formItemProps: {
+      hideLabel: true,
+    },
+    props: {
+      placeholder: '用户名/昵称/描述',
+    },
+  },
+  {
+    type: 'select',
+    field: 'status',
+    options: DisEnableStatusList,
+    formItemProps: {
+      hideLabel: true,
+    },
+    props: {
+      placeholder: '请选择状态',
+    },
+  },
+  {
+    type: 'range-picker',
+    field: 'createTime',
+    span: { lg: 2, xl: 2, xxl: 1 },
+    formItemProps: {
+      hideLabel: true,
+    },
+  },
+])
+
+const columns: TableInstanceColumns[] = [
+  {
+    title: '序号',
+    width: 66,
+    align: 'center',
+    render: ({ rowIndex }) => h('span', {}, rowIndex + 1 + (pagination.current - 1) * pagination.pageSize),
+    fixed: !isMobile() ? 'left' : undefined,
+  },
+  {
+    title: '昵称',
+    dataIndex: 'nickname',
+    slotName: 'nickname',
+    minWidth: 140,
+    ellipsis: true,
+    tooltip: true,
+    fixed: !isMobile() ? 'left' : undefined,
+  },
+  { title: '用户名', dataIndex: 'username', slotName: 'username', minWidth: 140, ellipsis: true, tooltip: true },
+  { title: '状态', slotName: 'status', align: 'center' },
+  { title: '性别', slotName: 'gender', align: 'center' },
+  { title: '所属部门', dataIndex: 'deptName', ellipsis: true, tooltip: true, minWidth: 180 },
+  { title: '角色', dataIndex: 'roleNames', minWidth: 160, slotName: 'roleNames' },
+  { title: '手机号', dataIndex: 'phone', minWidth: 170, ellipsis: true, tooltip: true },
+  { title: '邮箱', dataIndex: 'email', minWidth: 170, ellipsis: true, tooltip: true },
+  { title: '系统内置', slotName: 'isSystem', width: 100, align: 'center', show: false },
+  { title: '描述', dataIndex: 'description', minWidth: 130, ellipsis: true, tooltip: true },
+  { title: '创建人', dataIndex: 'createUserString', width: 140, ellipsis: true, tooltip: true, show: false },
+  { title: '创建时间', dataIndex: 'createTime', width: 180 },
+  { title: '修改人', dataIndex: 'updateUserString', width: 140, ellipsis: true, tooltip: true, show: false },
+  { title: '修改时间', dataIndex: 'updateTime', width: 180, show: false },
+  {
+    title: '操作',
+    slotName: 'action',
+    width: 190,
+    align: 'center',
+    fixed: !isMobile() ? 'right' : undefined,
+    show: has.hasPermOr(['system:user:update', 'system:user:delete', 'system:user:resetPwd']),
+  },
+]
+
+// 重置
+const reset = () => {
+  queryForm.description = undefined
+  queryForm.status = undefined
+  queryForm.createTime = []
+  search()
+}
+
+// 删除
+const onDelete = (record: UserResp) => {
+  return handleDelete(() => deleteUser(record.id), {
+    content: `是否确定删除 [${record.nickname}(${record.username})]？`,
+    showModal: true,
+  })
+}
+
+// 导出
+const onExport = () => {
+  useDownload(() => exportUser(queryForm))
+}
+
+// 根据选中部门查询
+const handleSelectDept = (keys: Array<any>) => {
+  queryForm.deptId = keys.length === 1 ? keys[0] : undefined
+  search()
+}
+
+const UserAddDrawerRef = ref<InstanceType<typeof UserAddDrawer>>()
+// 新增
+const onAdd = () => {
+  UserAddDrawerRef.value?.onAdd()
+}
+
+const UserImportDrawerRef = ref<InstanceType<typeof UserImportDrawer>>()
+// 导入
+const onImport = () => {
+  UserImportDrawerRef.value?.onImport()
+}
+
+// 修改
+const onUpdate = (record: UserResp) => {
+  UserAddDrawerRef.value?.onUpdate(record.id)
+}
+
+const UserDetailDrawerRef = ref<InstanceType<typeof UserDetailDrawer>>()
+// 详情
+const onDetail = (record: UserResp) => {
+  UserDetailDrawerRef.value?.onDetail(record.id)
+}
+
+const UserResetPwdModalRef = ref<InstanceType<typeof UserResetPwdModal>>()
+// 重置密码
+const onResetPwd = (record: UserResp) => {
+  UserResetPwdModalRef.value?.onReset(record.id)
+}
+</script>
+
+<style lang="scss" scoped>
+.page_header {
+  flex: 0 0 auto;
+}
+
+.page_content {
+  flex: 1;
+  overflow: auto;
+}
+</style>
