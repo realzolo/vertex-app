919e14d88198c01500d3deeda0dfe07bc33a2706
lixm <zolo@onezol.com>
2025-05-12 22:13:46 +0800

    fix: 修改优化信息报错

--- vertx-app-service/vertx-framework/vertx-framework-security/vertx-framework-security-api/src/main/java/com/onezol/vertx/framework/security/api/model/payload/UserSavePayload.java
+++ vertx-app-service/vertx-framework/vertx-framework-security/vertx-framework-security-api/src/main/java/com/onezol/vertx/framework/security/api/model/payload/UserSavePayload.java
@@ -25,8 +25,8 @@ public class UserSavePayload {
     private String nickname;
 
     @Schema(name = "密码")
-    @NotBlank(message = "密码不能为空")
-    @Size(min = 6, max = 25, message = "密码长度必须在6-25位之间")
+//    @NotBlank(message = "密码不能为空")
+//    @Size(min = 6, max = 25, message = "密码长度必须在6-25位之间")
     private String password;
 
     @Schema(name = "手机号码")
