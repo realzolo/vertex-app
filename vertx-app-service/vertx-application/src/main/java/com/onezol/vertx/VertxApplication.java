fde6d53cc7cda052493eabf03023074321ef8954
lixm <zolo@onezol.com>
2025-06-04 15:46:17 +0800

    refactor: 优化日志配置和启动输出
    - 在 logback-spring.xml 中为 FILE_MAX_HISTORY 添加默认值30 天
    - 修改 VertxApplication 类的 main 方法输出，移除 logger 依赖
    - 更新启动输出内容，增加 API 接口文档和 Actuator 健康检查链接

--- vertx-app-service/vertx-application/src/main/java/com/onezol/vertx/VertxApplication.java
+++ vertx-app-service/vertx-application/src/main/java/com/onezol/vertx/VertxApplication.java
@@ -1,8 +1,6 @@
 package com.onezol.vertx;
 
 import com.onezol.vertx.framework.common.constant.GenericConstants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,11 +10,8 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.TimeZone;
 
-@SuppressWarnings("ALL")
 @SpringBootApplication
 public class VertxApplication {
-
-    private static final Logger logger = LoggerFactory.getLogger(VertxApplication.class);
 
     public static void main(String[] args) throws UnknownHostException {
         TimeZone.setDefault(TimeZone.getTimeZone(GenericConstants.DEFAULT_TIME_ZONE));
@@ -25,11 +20,16 @@ public class VertxApplication {
         Environment env = context.getEnvironment();
         String ip = InetAddress.getLocalHost().getHostAddress();
         String port = env.getProperty("server.port", "8080");
-        String path = env.getProperty("server.servlet.context-path", "");
 
-        logger.info("""
-                Vertx App is running! 控制台首页: http://{ip}:{port} Swagger文档: http://{ip}:{port}/doc.html 
-                """.replaceAll("\\{ip}", ip).replaceAll("\\{port}", port));
+        System.out.println(
+                """
+                         -------------------------------- Vertx App --------------------------------
+                         Vertx App is running!
+                         API接口文档: http://{ip}:{port}/doc.html
+                         Actuator健康检查: http://{ip}:{port}/actuator/health
+                         ---------------------------------------------------------------------------
+                        """.replaceAll("\\{ip}", ip).replaceAll("\\{port}", port)
+        );
     }
 
 }
